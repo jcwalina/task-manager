83044cfd956ad3b08571e9f185b4e12aa4d867f1
jcwalina <joannacwalinaa@gmail.com>
2024-12-11 18:27:34 +0100

    fix: Drag & Drop

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
@@ -6,11 +5,9 @@ import { CustomThemeProvider } from './context/ThemeContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
         <AuthProvider>
             <CustomThemeProvider>
                 <App />
             </CustomThemeProvider>
         </AuthProvider>
-    </React.StrictMode>
 );
